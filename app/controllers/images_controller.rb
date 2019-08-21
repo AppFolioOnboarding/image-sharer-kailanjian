bea99e84eb633cec475d1643096ed264a28d66d9
Krikor Ailanjian <krikor.ailanjian@appfolio.com>
2019-08-21 15:07:01 -0700

    ka - add create action

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,4 +3,16 @@ class ImagesController < ApplicationController
     @image = Image.new
   end
 
+  def create
+    image_link = params[:image][:link]
+
+    @image = Image.new(link: image_link)
+
+    if @image.save
+      flash[:success] = 'Successfully saved image'
+      redirect_to @image
+    else
+      render new_image_path, status: :unprocessable_entity
+    end
+  end
 end
--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -8,4 +8,23 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     assert_select 'h1', 'Image link submission'
     assert_select 'form'
   end
+
+  def test_create
+    assert_difference 'Image.count' do
+      post images_path, params: { image: { link: 'http://www.example.com/image.png' } }
+    end
+
+    assert_redirected_to image_path(Image.last)
+    assert_equal flash[:success], 'Successfully saved image'
+  end
+
+  def test_create__invalid_url
+    assert_no_difference 'Image.count' do
+      post images_path, params: { image: { link: 'invalid' } }
+    end
+
+    assert_response :unprocessable_entity
+    assert_select '.invalid-feedback', 'Link is invalid'
+  end
+
 end
