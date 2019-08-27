7839e0afb53dc281275559f48afad04c8b650158
Krikor Ailanjian <krikor.ailanjian@appfolio.com>
2019-08-27 13:45:30 -0700

    ka - add form

--- app/javascript/image_sharing/components/App.js
+++ app/javascript/image_sharing/components/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Container, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import Header from './Header';
 
 export default function App() {
   return (
     <div>
-      <Header title="Tell us what you think" />
-      Put your components here: Flash Message, Form, Footer
+      <Container>
+        <Header title="Tell us what you think" />
+        <Form>
+          <FormGroup>
+            <Label for="nameInput">Your Name:</Label>
+            <Input name="name" id="nameInput" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="commentTextArea">Comments:</Label>
+            <Input type="textarea" id="commentTextArea" />
+          </FormGroup>
+          <Button color="primary">Submit</Button>
+        </Form>
+      </Container>
     </div>
   );
 }
--- app/javascript/image_sharing/test/components/App.spec.js
+++ app/javascript/image_sharing/test/components/App.spec.js
@@ -9,5 +9,8 @@ describe('<App />', () => {
   it('should render correctly', () => {
     const wrapper = mount(<App />);
     assert(wrapper.contains('Tell us what you think'));
+    assert(wrapper.exists('form'));
+    assert.strictEqual(wrapper.find('.form-group').length, 2);
+    assert(wrapper.find('footer').contains('Copyright: Appfolio Inc. Onboarding'));
   });
 });
