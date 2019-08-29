3e3ab576b5e75d2e62f39ca700351ecf2e3e8ae0
Krikor Ailanjian <krikor.ailanjian@appfolio.com>
2019-08-29 14:51:43 -0700

    ka - convert functional component to have state

--- app/javascript/image_sharing/components/App.js
+++ app/javascript/image_sharing/components/App.js
@@ -2,28 +2,62 @@ import React from 'react';
 import { Container, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import Header from './Header';
 
-export default function App() {
-  return (
-    <div>
-      <Container>
-        <Header title="Tell us what you think" />
-        <Form>
-          <FormGroup>
-            <Label for="nameInput">Your Name:</Label>
-            <Input name="name" id="nameInput" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="commentTextArea">Comments:</Label>
-            <Input type="textarea" id="commentTextArea" />
-          </FormGroup>
-          <Button color="primary">Submit</Button>
-        </Form>
-        <footer className="font-small text-center">
-          <small>Copyright: Appfolio Inc. Onboarding</small>
-        </footer>
-      </Container>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nameInput: '',
+      commentsInput: '',
+      status: 'none'
+    };
+    this.handleNameInputChange.bind(this);
+  }
+
+  handleSubmit = (event) => {
+  };
+
+   handleNameInputChange = (event) => {
+     this.setState({ nameInput: event.target.value });
+   };
+
+  handleCommentsInputChange = (event) => {
+    this.setState({ commentsInput: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <Container>
+          <Header title="Tell us what you think" />
+          <Form action="/api/feedbacks" onSubmit={this.handleSubmit} method="POST">
+            <FormGroup>
+              <Label for="nameInput">Your Name:</Label>
+              <Input
+                name="name"
+                id="nameInput"
+                onChange={this.handleNameInputChange}
+                value={this.state.nameInput}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="commentsTextArea">Comments:</Label>
+              <Input
+                name="comments"
+                type="textarea"
+                id="commentsTextArea"
+                onChange={this.handleCommentsInputChange}
+                value={this.state.commentsInput}
+              />
+            </FormGroup>
+            <Button id="formSubmitButton" color="primary">Submit</Button>
+          </Form>
+          <footer className="font-small text-center">
+            <small>Copyright: Appfolio Inc. Onboarding</small>
+          </footer>
+        </Container>
+      </div>
+    );
+  }
 }
 
 /* TODO: Add Prop Types check*/
